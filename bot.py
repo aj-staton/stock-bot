584df33fc12275f0f047fb7392bbdeb7e65d808c
aj-staton <ajstaton0@gmail.com>
2020-08-21 15:48:46 -0400

    Makefile

---
+++ bot.py
@@ -0,0 +1,9 @@
+# This script will grab the current price of stocks,
+# compare to analyst's price targets, and give those
+# with the highest price/target ratio.
+#
+# Written by: Austin Staton
+# Date: Aug 21st, 2020
+
+import requests
+
