dd09ac062bb5112ae0790e04faab90f14bb787ce
aj-staton <ajstaton0@gmail.com>
2020-08-21 16:29:48 -0400

    stopping point: can retrieve all tickers:

--- bot.py
+++ bot.py
@@ -5,5 +5,30 @@
 # Written by: Austin Staton
 # Date: Aug 21st, 2020
 
-import requests
+import requests as req
+from keys import *
+import lib
+import json
 
+LIMIT_CALLS = True
+
+# @breif: updateStockList() will update the group of Stocks
+#         collected in from the `symbol` API call. This is done
+#         in this function once, optionally, because I want to
+#         limit calls to the API.
+def updateStockList():
+    # Grab a list of supported stocks by the API
+    stocks = req.get(FINN + '/symbol?exchange=US&token=' + KEY)
+    with open("stock_list.json", "w") as f:
+        json.dump(stocks.json(), f)
+
+if __name__ == '__main__':
+    if (not LIMIT_CALLS):
+        updateStockList()
+
+    data = open("stock_list.json",)
+    stocks = json.load(data)
+    data.close()
+    
+    for stock in stocks['symbol']:
+        print(stock)
---
+++ lib.py
@@ -0,0 +1,8 @@
+# A group of supporting functions the stock bot might use
+
+# Written by: Austin Staton
+# Date: Aug 21st, 2020
+
+def printList(items):
+    for item in items:
+        print(item)
