b97ea5d87423c716c8bd3056043b275234cb3692
aj-staton <ajstaton0@gmail.com>
2020-08-22 20:58:47 -0400

    the bot is running, should take about 5 hours (for the rate limit at 9000 requests)

--- bot.py
+++ bot.py
@@ -9,26 +9,60 @@ import requests as req
 from keys import *
 import lib
 import json
+import time
 
+FIN = 'https://finnhub.io/api/v1' # API's URL
 LIMIT_CALLS = True
+LIMIT_TIME = 30 # My free key gets thirty calls per second. This will throttle. 
+PRICE_DIFFS = dict()
 
 # @breif: updateStockList() will update the group of Stocks
-#         collected in from the `symbol` API call. This is done
-#         in this function once, optionally, because I want to
-#         limit calls to the API.
+#         collected in from the `symbol` API call.
 def updateStockList():
     # Grab a list of supported stocks by the API
-    stocks = req.get(FINN + '/symbol?exchange=US&token=' + KEY)
+    stocks = req.get(FIN + '/stock/symbol?exchange=US&token=' + KEY)
     with open("stock_list.json", "w") as f:
         json.dump(stocks.json(), f)
 
 if __name__ == '__main__':
-    if (not LIMIT_CALLS):
-        updateStockList()
+    updateStockList()
 
     data = open("stock_list.json",)
     stocks = json.load(data)
     data.close()
     
-    for stock in stocks['symbol']:
-        print(stock)
+    write_file = open('data_dump.csv', 'w+')
+    write_file.write('symbol,current_price,target-price,difference')
+    write_file.close()
+    write_file = open ('data_dump.csv', 'a')
+
+    # Go through all stock tickers and look for analyst projects (if existing).
+    for stock in stocks:
+       
+        symbol = stock['symbol']
+        
+        # Grab Pricing data.
+        r = req.get(FIN+'/quote?symbol='+symbol+'&token='+KEY)
+        if (r.status_code == 200):
+            try:
+                price = r.json()['c'] # 'c' is current price.
+            except:
+                continue
+        else:
+            print("Bad HTTP Request. Check URL and API call limits.")
+            exit(0)
+        
+        # Grab Analyst Target Data (the median, specifically)
+        r = req.get(FIN+'/stock/price-target?symbol='+symbol+'&token='+KEY)
+        if (r.status_code == 200):
+            try:
+                target = r.json()['targetMedian']
+            except:
+                continue
+
+        write_file.write(symbol+","+str(price)+ ","+str(target)+","+\
+                            str(target-price))
+        
+        time.sleep(2) # This is a rate-limited API.
+
+    write_file.close()
